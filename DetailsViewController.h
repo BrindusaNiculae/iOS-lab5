85e2c4bed20379540eb4103eed5feb06d25d12ea
ios5 <ios5@serbans-mbp.ad.qualitance.com>
2015-07-10 12:22:46 +0300

    detailsView

---
+++ DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  Places
+//
+//  Created by ios5 on 7/10/15.
+//  Copyright (c) 2015 Brindusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+
+@end
