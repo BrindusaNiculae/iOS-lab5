8986c11a5114ecd7ea96bf6aac58348301d053e5
ios5 <ios5@serbans-mbp.ad.qualitance.com>
2015-07-10 15:14:02 +0300

    lab9

---
+++ PositionManager.h
@@ -0,0 +1,13 @@
+//
+//  PositionManager.h
+//  Places
+//
+//  Created by ios5 on 7/10/15.
+//  Copyright (c) 2015 Brindusa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PositionManager : NSObject
+
+@end
---
+++ PositionManager.m
@@ -0,0 +1,13 @@
+//
+//  PositionManager.m
+//  Places
+//
+//  Created by ios5 on 7/10/15.
+//  Copyright (c) 2015 Brindusa. All rights reserved.
+//
+
+#import "PositionManager.h"
+
+@implementation PositionManager
+
+@end
